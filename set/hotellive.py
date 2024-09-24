eeb0d9261e48d6fcea44c668d68ec15011fe6f6a
zhuxinfu88 <zhuxinfu@sohu.com>
2024-09-24 12:16:29 +0800

    Update hotellive.py

--- set/hotellive.py
+++ set/hotellive.py
@@ -348,7 +348,8 @@ def main():
     print(len(sources))
     if len(sources) < 500:
         ip_addresses_beijing = make_request("绍兴")
-        
+        ip_addresses_CHC = make_request("CMC")
+        ip_addresses = ip_addresses_beijing + ip_addresses_CHC
         with open("./itv.txt", 'w', encoding='utf-8') as file:
             with ThreadPoolExecutor(max_workers=10) as executor:
                 futures = [executor.submit(parse_channels_and_sources, ip, file) for ip in ip_addresses]
