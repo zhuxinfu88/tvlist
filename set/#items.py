f1a588646c68528aff98cb39dd85dc43527c840e
zhuxinfu88 <zhuxinfu@sohu.com>
2024-10-02 12:28:43 +0800

    Update #items.py

--- set/#items.py
+++ set/#items.py
@@ -61,15 +61,14 @@ for keyword in keywords:
         
 #    else:
 #        org = ""
-    province = "浙江"
-    org = "Chinanet"
+    
     current_time = datetime.now()
     timeout_cnt = 0
     result_urls = set() 
     while len(result_urls) == 0 and timeout_cnt <= 5:
         try:
             search_url = 'https://fofa.info/result?qbase64='
-            search_txt = f'\"udpxy\" && country=\"CN\" && region=\"{province}\" && org=\"{org}\" && city=\"NingBo\" '
+            search_txt = f'\"udpxy\" && country=\"CN\" && region=\"{province}\" && org=\"{org}\"'
             bytes_string = search_txt.encode('utf-8')
 
             search_txt = base64.b64encode(bytes_string).decode('utf-8')
