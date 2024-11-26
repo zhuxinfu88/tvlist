c720e79803c2cabb44be2b48f7b5d6447c0af8e8
zhuxinfu88 <zhuxinfu@sohu.com>
2024-11-26 21:23:23 +0800

    Add files via upload

---
+++ libs/juquan.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8"/>
+  <link rel="icon" href="/favicon.ico">
+  <meta name="renderer" content="webkit"/>
+  <meta name="force-rendering" content="webkit"/>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+  <meta name="viewport" content="width=device-width"/>
+  <meta name="data-fact" content="clouddrive">
+  <meta name="spm-id" content="clouddrive">
+  <meta name="data-spm" content="clouddrive"/>
+  <meta name="aplus-waiting" content="MAN">
+  <meta name="baidu-site-verification" content="codeva-RGQB5FUf7i" />
+  <meta name="multiple-page">
+  <script>
+    window.__globalData = { uidWsg: '' };
+  </script>
+  
+  <script>
+    window.__WPO = window.__WPO || {};
+    window.__WPO.config = {
+      sample: 1,
+      spmId: 'clouddriveweb.share',
+      startTime: Date.now(),
+      dynamic: false,
+    };
+  </script>
+  
+  
+  <title>夸克网盘分享</title>
+  <meta name="description" content="夸克网盘是夸克推出的一款云服务产品，功能包括云存储、高清看剧、文件在线解压、PDF一键转换等。通过夸克网盘可随时随地管理和使用照片、文档、手机资料，目前支持Android、iOS、PC、iPad。">
+  <meta name="keywords" content="夸克网盘,云存储,云同步,云盘,文件管理,相册备份,手机备份,高清播放,上传下载">
+  <link rel="shortcut icon" type="image/vnd.microsoft.icon" href="/favicon.ico"/>
+  <link href="//g.alicdn.com/uc-cloud-drive-web-system/cloud-drive-web/4.3.159/vendor.css" rel="stylesheet">
+  <link href="//g.alicdn.com/uc-cloud-drive-web-system/cloud-drive-web/4.3.159/share.css" rel="stylesheet">
+  <script>
+    window.__LOCAL_VER = '4.3.159';
+    window.__tenant = 'quark';
+  </script>
+</head>
+<body>
+<div id="ice-container"></div>
+
+
+<script src="//g.alicdn.com/retcode/log/log.js" crossorigin></script>
+
+<script>
+  window.globalConfig = {"share_config":{"loginBanner":"https://yes-file.uc.cn/file/test/1692349517242_3536355815_6405.png","saveModal":{"imgUrl":"https://image.quark.cn/s/uae/g/3o/cms/resource/1702472977876_3804915593_4590.png","imgUrlForPCQuark":"https://yes-file.quark.cn/file/1710317237640_2936189756_4343.png"},"saveModal4New":{"imgUrl":"https://image.quark.cn/s/uae/g/3o/broccoli/resource/202404/40dcf700-fdf2-11ee-aaec-b115fa1ad054.png","imgUrlForPCQuark":"https://image.quark.cn/s/uae/g/3o/broccoli/resource/202404/40dcf700-fdf2-11ee-aaec-b115fa1ad054.png"},"openClientIMg":{"imgUrl":"https://g.alicdn.com/uc-cloud-drive-web-system/cloud-drive-web/4.3.158/assets/f5ca5c29f13defcc433546b79c214ab2.png","imgUrlForPCQuark":"https://g.alicdn.com/uc-cloud-drive-web-system/cloud-drive-web/4.3.158/assets/f5ca5c29f13defcc433546b79c214ab2.png"},"featureScreens":{"driveConfigs":[{"title":"智能备份\\n把电脑搬到夸克网盘","description":"重要办公学习资料怕丢失？网盘备份文件时电脑变慢？\\n 登录夸克网盘开启备份，支持微信/QQ/桌面/文档/下载\\n备份和指定文件夹备份；\\n夸克网盘独有三种智能备份模式，低耗能不卡顿。","imageUrl":"https://image.quark.cn/s/uae/g/3o/broccoli/resource/202311/d0c98cb0-82e7-11ee-942b-1317b378239d.jpeg"},{"title":"大屏播放更沉浸\\n新升级AI字幕","description":"最高支持4K高清画质，屏幕能支持多高清，播放器体验就有多清晰；还有高阶配置，支持多音轨和多字幕；倍速0.5-5随心调，高效学习还免费；\\n新升级AI字幕，学习课件自动生成字幕，英语材料直接翻译，并导出成文稿。","imageUrl":"https://image.quark.cn/s/uae/g/3o/broccoli/resource/202311/cd0972d0-8368-11ee-8302-1dc72091cc01.jpeg"},{"title":"夸克快传\\n大文件无损传输，永久保留不过期","description":"支持最大100GB文件发送，批量发也很方便；文件没有时间限制，永不过期～\\n不仅能发给自己的其他设备，还可以邀请朋友创建群组，不管是本地文件还是网盘文件，都可以一键发送，不压缩文件，保留所有原始参数。","imageUrl":"https://image.quark.cn/s/uae/g/3o/broccoli/resource/202404/91242080-f712-11ee-a33c-cd6c1a684ab0.png"},{"title":"易用的PDF阅读编辑\\n文件无需下载在线处理","description":"页面自由添加提取，轻松拆文件\\n画笔涂鸦高亮标注，快速标识重点","imageUrl":"https://image.quark.cn/s/uae/g/3o/broccoli/resource/202311/1d9feb20-82e7-11ee-942b-1317b378239d.jpeg"},{"title":"一键转换PDF\\n高保真还原排版","description":"将PDF转换成Word、Excel、PPT、图片，快速精准，\\n多文档也能批量上传和转换，让工作更高效","imageUrl":"https://image.quark.cn/s/uae/g/3o/broccoli/resource/202311/e90c02e0-8368-11ee-8302-1dc72091cc01.jpeg"},{"title":"136种格式在线预览\\n无需下载马上查看","description":"多种音视频，图片，文档，笔记，代码文件在线预览；\\nCAD工程图，psd设计图，xmind脑图，json代码等无需下载，直接查看！","imageUrl":"https://image.quark.cn/s/uae/g/3o/broccoli/resource/202311/8377d930-82e7-11ee-942b-1317b378239d.jpeg"}],"quarkConfigs":[{"title":"夸克搜索，一键直达","description":"精准高效，搜索问题更快，AI智能引擎\\\\n查资料更快，工作、学习文档精准搜","imageUrl":"https://yes-file.uc.cn/file/test/1710137738361_1950658521_3981.png"},{"title":"全面保护，极速安全","description":"强大的安全和隐私保护功能，高兼容性\\\\n丰富的智能网页工具提升你的浏览效率和体验","imageUrl":"https://yes-file.uc.cn/file/test/1710137780594_994799076_1305.png"},{"title":"夸克网盘，就是爽快","description":"6T超大空间，超速下载\\\\n电脑资料安心存，多端备份\\\\n超级播放器，免费5倍速","imageUrl":"https://yes-file.uc.cn/file/test/1709894711394_2084687691_7120.png"},{"title":"文件工具中心","description":"支持PFG文档转换为Word、Excel、PPT、txt、图片等格式，高保真还原排版，轻松查看和编辑在线文档","imageUrl":"https://yes-file.uc.cn/file/1710389459833_2084687691_8538.png"},{"title":"夸克快传，无损传输","description":"多端贡献，文件急速传输。文档、图片、视频、大文件、一键无损快传，手机与电脑轻松连接","imageUrl":"https://yes-file.uc.cn/file/test/1710137919143_4166845277_2491.png"}]},"rightSideBanners":[{"link":"https://www.yuque.com/docs/share/a6aacd06-6284-4846-acc8-a1c1aa236515?#","isDownload":true,"linkForPCQuark":"https://www.yuque.com/docs/share/a6aacd06-6284-4846-acc8-a1c1aa236515?#","isDownloadForPCQuark":true,"imgUrl":"https://image.quark.cn/s/uae/g/3o/cms/resource/1702472767194_2896046663_9346.png","imgUrlForPCQuark":"https://image.quark.cn/s/uae/g/3o/cms/resource/1706280387041_1109495036_8525.png"},{"isDownload":true,"isDownloadForPCQuark":true,"imgUrl":"https://image.quark.cn/s/uae/g/3o/cms/resource/1702472977876_3804915593_4590.png","imgUrlForPCQuark":"https://image.quark.cn/s/uae/g/3o/cms/resource/1706280401112_4093230283_4362.png"}]},"buwang_ui_config":{"entry":"default","appch":"pckk@other_ch","backgroundColor":"#f3f6fe","functionScreen":{"title":"夸克网盘\\n电脑高效拍档  一键存爽快看","backgroundImage":"https://image.quark.cn/s/uae/g/3o/broccoli/resource/202311/1c69d780-8368-11ee-8302-1dc72091cc01.png","featureLabel":[{"iconUrl":"https://image.quark.cn/s/uae/g/3o/broccoli/resource/202311/d83f9750-82e2-11ee-942b-1317b378239d.png","label":"超大空间，超速下载"},{"iconUrl":"https://image.quark.cn/s/uae/g/3o/broccoli/resource/202311/dc543530-82e2-11ee-942b-1317b378239d.png","label":"PDF一键转换、编辑"},{"iconUrl":"https://image.quark.cn/s/uae/g/3o/broccoli/resource/202311/e02d9ed0-82e2-11ee-942b-1317b378239d.png","label":"超级播放器，升级AI字幕"}]},"featureScreens":[{"title":"智能备份\\n把电脑搬到夸克网盘","description":"重要办公学习资料怕丢失？网盘备份文件时电脑变慢？\\n 登录夸克网盘开启备份，支持微信/QQ/桌面/文档/下载\\n备份和指定文件夹备份；\\n夸克网盘独有三种智能备份模式，低耗能不卡顿。","imageUrl":"https://image.quark.cn/s/uae/g/3o/broccoli/resource/202311/d0c98cb0-82e7-11ee-942b-1317b378239d.jpeg"},{"title":"大屏播放更沉浸\\n新升级AI字幕","description":"最高支持4K高清画质，屏幕能支持多高清，播放器体验就有多清晰；还有高阶配置，支持多音轨和多字幕；倍速0.5-5随心调，高效学习还免费；\\n新升级AI字幕，学习课件自动生成字幕，英语材料直接翻译，并导出成文稿。","imageUrl":"https://image.quark.cn/s/uae/g/3o/broccoli/resource/202311/cd0972d0-8368-11ee-8302-1dc72091cc01.jpeg"},{"title":"夸克快传\\n大文件无损传输，永久保留不过期","description":"支持最大100GB文件发送，批量发也很方便；文件没有时间限制，永不过期～\\n不仅能发给自己的其他设备，还可以邀请朋友创建群组，不管是本地文件还是网盘文件，都可以一键发送，不压缩文件，保留所有原始参数。","imageUrl":"https://image.quark.cn/s/uae/g/3o/broccoli/resource/202404/91242080-f712-11ee-a33c-cd6c1a684ab0.png"},{"title":"易用的PDF阅读编辑\\n文件无需下载在线处理","description":"页面自由添加提取，轻松拆文件\\n画笔涂鸦高亮标注，快速标识重点","imageUrl":"https://image.quark.cn/s/uae/g/3o/broccoli/resource/202311/1d9feb20-82e7-11ee-942b-1317b378239d.jpeg"},{"title":"一键转换PDF\\n高保真还原排版","description":"将PDF转换成Word、Excel、PPT、图片，快速精准，\\n多文档也能批量上传和转换，让工作更高效","imageUrl":"https://image.quark.cn/s/uae/g/3o/broccoli/resource/202311/e90c02e0-8368-11ee-8302-1dc72091cc01.jpeg"},{"title":"下载速度快\\nSVIP高达50MB/s","description":"大文件下载再也不用等，\\n日常转发的课件资料、办公软件，随用随下；\\n还支持解析磁力链和BT种子，大文件一件存网盘","imageUrl":"https://image.quark.cn/s/uae/g/3o/broccoli/resource/202311/ac75ed90-82e7-11ee-942b-1317b378239d.jpeg"},{"title":"136种格式在线预览\\n无需下载马上查看","description":"多种音视频，图片，文档，笔记，代码文件在线预览；\\nCAD工程图，psd设计图，xmind脑图，json代码等无需下载，直接查看！","imageUrl":"https://image.quark.cn/s/uae/g/3o/broccoli/resource/202311/8377d930-82e7-11ee-942b-1317b378239d.jpeg"}],"pcLogin":{"imageUrl":"https://image.quark.cn/s/uae/g/3o/broccoli/resource/202311/48d01a70-7ad3-11ee-9872-11ffbbb46c2f.png"},"pcLoginSrc":[{"key":"pc_bt","imageUrl":"https://image.quark.cn/s/uae/g/3o/broccoli/resource/202311/4dedeaf0-7ad3-11ee-9872-11ffbbb46c2f.png"},{"key":"plugin","imageUrl":"https://image.quark.cn/s/uae/g/3o/broccoli/resource/202311/38cc5f30-7ad3-11ee-9872-11ffbbb46c2f.png"},{"key":"magnet_right_click","imageUrl":"https://image.quark.cn/s/uae/g/3o/broccoli/resource/202311/38cc5f30-7ad3-11ee-9872-11ffbbb46c2f.png"}]},"defaultLoginWay":[{"scene":"web.home","scan":90,"mobile":0,"wx":10},{"scene":"web.share","scan":0,"mobile":90,"wx":10},{"scene":"desktop.official","scan":100,"mobile":0,"wx":0},{"scene":"desktop.store","scan":90,"mobile":0,"wx":10}]};
+</script>
+<script type="text/javascript" src="//g.alicdn.com/uc-cloud-drive-web-system/cloud-drive-web/4.3.159/vendor.js" crossorigin></script>
+<script type="text/javascript" src="//g.alicdn.com/uc-cloud-drive-web-system/cloud-drive-web/4.3.159/share.js" crossorigin></script>
+</body>
+</html>
