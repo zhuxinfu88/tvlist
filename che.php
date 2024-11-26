77e749d2980e0b172fc4952878cd33e9a93e412d
zhuxinfu88 <zhuxinfu@sohu.com>
2024-11-26 18:24:08 +0800

    Add files via upload

---
+++ che.php
@@ -0,0 +1,270 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>vip.lyyytv.cn 影探在手看撒都有</title>
+    <link rel="shortcut icon" href="/assets/img/favicon.ico" />
+    <style>
+        html,
+        body {
+            height: 100%;
+            width: 100%;
+        }
+
+        * {
+            -webkit-box-sizing: border-box;
+            -moz-box-sizing: border-box;
+            box-sizing: border-box;
+        }
+
+        body {
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+            font-family: "Roboto", "SF Pro SC", "SF Pro Display", "SF Pro Icons", "PingFang SC", BlinkMacSystemFont, -apple-system, "Segoe UI", "Microsoft Yahei", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", "Helvetica", "Arial", sans-serif;
+            font-weight: 400;
+            margin: 0;
+        }
+
+        a {
+            -webkit-transition: all 0.35s;
+            -moz-transition: all 0.35s;
+            transition: all 0.35s;
+            color: #474157;
+        }
+
+        a:hover,
+        a:focus {
+            color: #474157;
+        }
+
+        hr {
+            max-width: 100px;
+            margin: 25px auto 0;
+            border-width: 1px;
+            border-color: rgba(34, 34, 34, 0.1);
+        }
+
+        hr.light {
+            border-color: white;
+        }
+
+        h1,
+        h2,
+        h3,
+        h4,
+        h5,
+        h6 {
+            font-weight: 200;
+            letter-spacing: 1px;
+        }
+
+        p {
+            font-size: 16px;
+            line-height: 1.5;
+            margin-bottom: 20px;
+        }
+
+        #mainbody {
+            position: relative;
+            width: 100%;
+            min-height: auto;
+            overflow-y: hidden;
+            background: #f1f6fd;
+            color: #474157;
+            /* height: calc(100vh - 61px); */
+            /* 背景图片 */
+            background-image: url('图片地址');
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+
+        #mainbody>.container>.text-center {
+            text-align: center;
+            padding: 350px 0 50px;
+            position: relative;
+            height: 100vh;
+        }
+
+        #mainbody>.container>.text-center h1 {
+            font-size: 50px;
+            font-weight: bold;
+            color: #171347
+        }
+
+        #mainbody>.container>.text-center>a {
+            background: #fdcc52;
+            background: -webkit-linear-gradient(#fdcc52, #fdc539);
+            background: linear-gradient(#fdcc52, #fdc539);
+            text-transform: uppercase;
+            letter-spacing: 2px;
+            border-radius: 300px;
+            margin-top: 20px;
+            padding: 10px 45px;
+            font-size: 14px;
+            text-decoration: none;
+        }
+
+        .open-tip {
+            background: #fdcc52;
+            background: -webkit-linear-gradient(#fdcc52, #fdc539);
+            background: linear-gradient(#fdcc52, #fdc539);
+            text-transform: uppercase;
+            letter-spacing: 2px;
+            border-radius: 300px;
+            padding: 10px 45px;
+            font-size: 14px;
+            text-decoration: none;
+        }
+
+        .mobile-btn-list {
+            flex-direction: column;
+            align-items: center;
+            display: none;
+        }
+
+        .pc-btn-list {
+            padding: 20px;
+            display: flex;
+            flex-direction: column;
+        }
+
+        .pc-btn-list a,
+        .mobile-btn-list a {
+            margin-bottom: 30px;
+            display: inline-block;
+        }
+
+        .pc-btn-list a:nth-child(1),
+        .mobile-btn-list a:nth-child(1) {
+            color: red;
+        }
+
+        .pc-btn-list a:nth-child(2),
+        .mobile-btn-list a:nth-child(2) {
+            color: blue;
+        }
+
+        .img-container{
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            margin-bottom: 10px;
+        }
+
+        img {
+            width: 80%;
+            height: auto;
+        }
+
+        @media (max-height: 500px) {
+            #mainbody {
+                height: inherit;
+            }
+        }
+
+        @media (min-width: 768px) {
+            .navbar-default {
+                background-color: transparent;
+                border-color: transparent;
+            }
+
+            #mainbody .index-text {
+                text-align: left;
+            }
+
+         
+        }
+
+        @media (max-width: 767px) {
+
+            #mainbody>.container>.text-center {
+                padding: 130px 0 0 0;
+                height: calc(100vh - 261px);
+            }
+
+            #mainbody>.container>.text-center>h1 {
+                font-size: 50px;
+                margin-bottom: 20px;
+            }
+
+            .mobile-btn-list {
+                display: flex;
+            }
+
+            .pc-btn-list {
+                display: none;
+            }
+        }
+
+        .footer {
+            background-color: #222222;
+            padding: 20px 0;
+            color: rgba(255, 255, 255, 0.3);
+            text-align: center;
+        }
+
+        .footer p {
+            font-size: 14px;
+            margin: 0;
+        }
+    </style>
+</head>
+<div class="Banner_InstallDoc__2UUMe"></div>
+<div class="Banner_InstallDoc__2UUMe">
+
+
+    <div id="mainbody">
+        <div class="container">
+            <div class="pc-btn-list">
+               
+                
+                <a href="/app/影探魔盒2.2.240407.apk" class="Banner_DownloadButton__30SHi">影探磨盒（需安卓5以上）</a>
+                
+                <a href="/app/影探手机竖屏版2.2.240114.apk" class="Banner_DownloadButton__30SHi">手机竖屏版（支持投屏）</a>
+                 
+                <a href="/app/影探4K2.2.240214.apk" class="Banner_DownloadButton__30SHi">影探4K通用版</a>
+                
+                
+        
+                
+                <a href="/app/使用教程.mp4" class="Banner_DownloadButton__30SHi">使用教程视频</a>
+            </div>
+            <div class="img-container">
+                <img src="./topimg.jpg" alt="">
+            </div>
+                
+    
+            <div class="mobile-btn-list">
+
+                   <a href="/app/影探魔盒2.2.240407.apk" class="Banner_DownloadButton__30SHi">
+                    <font size="5">影探魔盒（需安卓5以上）</font>
+                </a>
+               
+                   <a href="/app/影探手机竖屏版2.2.240114.apk" class="Banner_DownloadButton__30SHi">
+                    <font size="5">手机竖屏版（支持投屏）</font>
+                </a>
+               
+               
+                <a href="/app/影探4K2.2.240214.apk" class="Banner_DownloadButton__30SHi">
+                    <font size="6">影探4K通用版</font>
+                </a>
+                 
+               <a class="open-tip" href="http://vip.lyyytv.cn/index/user/index">影探魔盒和手机竖屏版账号互通</a>
+
+                <a class="open-tip" href="http://vip.lyyytv.cn/index/user/index">首次安装过5分钟后即可播放4K线路</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer">
+        <div class="container">
+            <p>Copyright @ 影探LTV 2023版权所有 <a href="https://lfytv.cn" target="_blank">影探你的观影帮手</a></p>
+        </div>
+    </div>
+    </body>
+</html>
