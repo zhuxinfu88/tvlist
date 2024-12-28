08196b11e3b75a4998543f4776768b358c6f531d
zhuxinfu88 <zhuxinfu@sohu.com>
2024-12-28 18:24:44 +0800

    Update IPTVmain.py

--- IPTVmain.py
+++ IPTVmain.py
@@ -150,9 +150,10 @@ def updateChannelUrlsM3U(channels, template_channels):
                                     base_url = url
 
                                 new_url = f"{base_url}{url_suffix}"
-
+                               
                                 f_m3u.write(f"#EXTINF:-1 tvg-id=\"{index}\" tvg-name=\"{channel_name}\" tvg-logo=\"https://gcore.jsdelivr.net/gh/yuanzl77/TVlogo@master/png/{channel_name}.png\" group-title=\"{category}\",{channel_name}\n")
                                 f_m3u.write(new_url + "\n")
+                                if not is_ipv6(new_url):
                                 f_txt.write(f"{channel_name},{new_url}\n")
 
             f_txt.write("\n")
