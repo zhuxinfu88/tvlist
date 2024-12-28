00606dfd030be6b06f77dba281899ab82b2f82d8
zhuxinfu88 <zhuxinfu@sohu.com>
2024-12-28 15:57:30 +0800

    Update IPTVmain.py

--- IPTVmain.py
+++ IPTVmain.py
@@ -141,9 +141,9 @@ def updateChannelUrlsM3U(channels, template_channels):
                             total_urls = len(filtered_urls)
                             for index, url in enumerate(filtered_urls, start=1):
                                 if is_ipv6(url):
-                                    url_suffix = f"$LR•IPV6" if total_urls == 1 else f"$LR•IPV6『线路{index}』"
+                                    url_suffix = f"$LR•IPV6" if total_urls == 1 else f"$6『线路{index}』"
                                 else:
-                                    url_suffix = f"$LR•IPV4" if total_urls == 1 else f"$LR•IPV4『线路{index}』"
+                                    url_suffix = f"$LR•IPV4" if total_urls == 1 else f"$4『线路{index}』"
                                 if '$' in url:
                                     base_url = url.split('$', 1)[0]
                                 else:
